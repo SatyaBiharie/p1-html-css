fa14abd09ad973ebe8d0e1b345361b71da764a56
SatyaBiharie <9021749@student.zadkine.nl>
2023-10-05 10:05:25 +0200

    javascript

---
+++ functions/app.js
@@ -0,0 +1,9 @@
+function changeColor() {
+    document.getElementById("title").style.display = "none";
+    document.getElementById("title1").innerText = "gone"
+}
+
+
+function changeFont(){
+    document.getElementById("title1").style.fontSize = "50px";
+}
