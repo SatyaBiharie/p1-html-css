32ae2a33d6b9b1e3585640312af4f8ab548ea890
SatyaBiharie <9021749@student.zadkine.nl>
2023-10-05 11:27:57 +0200

    opdracht 4.2

--- functions/app.js
+++
@@ -1,9 +0,0 @@
-function changeColor() {
-    document.getElementById("title").style.display = "none";
-    document.getElementById("title1").innerText = "gone"
-}
-
-
-function changeFont(){
-    document.getElementById("title1").style.fontSize = "50px";
-}
